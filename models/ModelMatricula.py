9ec0dcdd3c359a39a42f0cdb1983f6c01b141163
lucca3447 <lucca3447@hotmail.com>
2026-04-03 17:12:57 -0300

    New: Feito modelmatricula, repositorymatricula

--- models/ModelMatricula.py
+++ models/ModelMatricula.py
@@ -1,2 +1,15 @@
-class ModelMatricula:
-    pass
+from database import Base
+from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey
+
+class ModelMatricula(Base):
+    __tablename__ = "Matriculas"
+
+    id = Column(Integer,primary_key=True, index=True)
+    aluno_id = Column(Integer, ForeignKey("Alunos.id"), nullable=False)
+    turma_id = Column(Integer, ForeignKey("Turmas.id"), nullable=False)
+    data_matricula = Column(Date, nullable=False)
+    status = Column(String(20), nullable=False, default="ativa") #ou trancada, cancelada
+    ativo = Column(Boolean, nullable=False, default=True)
+
+
+   
