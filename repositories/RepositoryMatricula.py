a9c79db13e11e3a24a3ea50a32e74e7fab6c4505
lucca3447 <lucca3447@hotmail.com>
2026-04-03 17:12:57 -0300

    New: Feito modelmatricula, repositorymatricula

--- repositories/RepositoryMatricula.py
+++ repositories/RepositoryMatricula.py
@@ -1,2 +1,24 @@
-class RepositoryMatricula:
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
+
+class RepositoryMatriculaBase(BaseModel):
+    aluno_id: int
+    turma_id: int
+    data_matricula: date
+    status: str = Field(default="ativa", description="ativa, trancada ou cancelada")
+    ativo:bool = True
+
+class RepositoryMatriculaCreate(RepositoryMatriculaBase):
     pass
+
+class RepositoryMatriculaResponse(RepositoryMatriculaBase):
+    id:int
+    
+    class Config:
+        from_atributes = True
+
+class RepositoruMatriculaUpdate(BaseModel):
+    turma_id: Optional[int] = None
+    status: Optional[str] = None
+    ativo: Optional[bool] = None
