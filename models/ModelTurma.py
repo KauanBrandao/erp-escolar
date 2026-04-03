8d21934538ecbab8e703acd5c41d85623960e0f2
lucca3447 <lucca3447@hotmail.com>
2026-04-03 15:50:37 -0300

    Removido uvloop dos requirements

--- models/ModelTurma.py
+++ models/ModelTurma.py
@@ -1,2 +1,15 @@
-class ModelTurma:
-    pass
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+class ModelTurma(Base):
+    __tablename__ = "Turmas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String(50), nullable=False)
+    serie = Column(String(50), nullable=False)
+    turno = Column(String(20), nullable=False)
+    ano_letivo = Column(Integer, nullable=False)
+    ativo = Column(Boolean, nullable=False, default=True)
+    professor_id = Column(Integer, ForeignKey("Usuarios.id"), nullable=True)
+
+   
