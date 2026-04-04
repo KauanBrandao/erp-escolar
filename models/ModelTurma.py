5dc945dc911ad4adf5b2fb7613fb88e47e69445c
lucca3447 <lucca3447@hotmail.com>
2026-04-03 22:33:57 -0300

    feat: adicionados model nota e repository nota.   refactor: corrigido variaveis

--- models/ModelTurma.py
+++ models/ModelTurma.py
@@ -9,7 +9,6 @@ class ModelTurma(Base):
     serie = Column(String(50), nullable=False)
     turno = Column(String(20), nullable=False)
     ano_letivo = Column(Integer, nullable=False)
-    ativo = Column(Boolean, nullable=False, default=True)
     perfil_id = Column(Integer, ForeignKey("Usuarios.id"), nullable=True)
 
    
--- repositories/RepositoryNota.py
+++ repositories/RepositoryNota.py
@@ -1,2 +1,27 @@
-class RepositoryNota:
+from pydantic import BaseModel, Field
+from typing import Optional
+from decimal import Decimal
+from datetime import date
+
+class RepositoryNotaBase(BaseModel):
+    valor: Decimal = Field(..., ge=Decimal("0.0"), le=Decimal("10.0"), decimal_places=2)
+    tipo: str = Field(..., description="trabalho ou prova")
+    bimestre: int = Field(..., ge=1, le=4)
+    lancado_em:date
+    aluno_id: int
+    disciplina_id:int
+
+class RepositoryNotaCreate(RepositoryNotaBase):
     pass
+
+class RepositoryNotaResponse(RepositoryNotaBase):
+    id:int
+
+    class config:
+        from_attributes = True
+
+class RepositoryNotaUpdate(BaseModel):
+    valor: Optional[Decimal] = Field(None, ge=Decimal("0.0"), le=Decimal("10.0"), decimal_places=2)
+    tipo: Optional[str] = None
+    bimestre: Optional[int] = Field(None, ge=1, le=4)
+    lancado_em: Optional[date] = None
