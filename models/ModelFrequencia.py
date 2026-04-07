45207caca3665ebc600de60e49e930d2b1705a63
Artur Campos Câmara <117042395+Artur0campos@users.noreply.github.com>
2026-04-07 14:54:26 -0300

    new: criação do schema e model de frequencia

--- models/ModelFrequencia.py
+++ models/ModelFrequencia.py
@@ -1,2 +1,13 @@
-class ModelFrequencia:
-    pass
+from datetime import date
+
+from database import Base
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, Text
+
+
+class ModelFrequencia(Base):
+    id = Column(Integer, primary_key=True, index=True )
+    data_aula = Column(date, nullable=False)
+    presente = Column(Boolean, nullable=False)
+    justificativa = Column(Text, nullable=True)
+    disciplina_id = Column(Integer, ForeignKey("Disciplinas.id"), nullable=False)
+    aluno_id = Column(Integer, ForeignKey("Alunos.id"), nullable=False)
