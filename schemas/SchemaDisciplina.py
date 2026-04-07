b9a8a64212f23dc02810ea8944108adb4dd146db
Artur Campos Câmara <117042395+Artur0campos@users.noreply.github.com>
2026-04-07 14:41:28 -0300

    fix: corrigindo o local do codigo, repository -> schemas

--- schemas/SchemaDisciplina.py
+++ schemas/SchemaDisciplina.py
@@ -1,2 +1,26 @@
-class SchemaDisciplina:
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class DisciplinaBase(BaseModel):
+    nome: str = Field(..., min_length=3, max_length=100, description="Ex: Matemática")
+    codigo: str = Field(..., min_length=2, max_length=20, description="Ex: MAT01")
+    carga_horaria: int = Field(..., gt=0, description="Carga horária anual em horas")
+    turma_id: int
+    
+class DisciplinaCreate(DisciplinaBase):
     pass
+
+
+class DisciplinaResponse(DisciplinaBase):
+    id:int
+
+    class Config:
+        from_atributes = True
+
+class DisciplinaUptade(BaseModel):
+    nome: Optional[str] = None
+    codigo: Optional[str] = None
+    carga_horaria: Optional[int] = None
+    turma_id: Optional[int] = None
