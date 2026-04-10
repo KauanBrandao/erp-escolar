df006e5234fb727a7ce8897dfa8b4d68437a8371
Artur Campos Câmara <117042395+Artur0campos@users.noreply.github.com>
2026-04-10 17:06:08 -0300

    new: criação do schema e model de Comunicado

--- models/ModelComunicado.py
+++ models/ModelComunicado.py
@@ -1,2 +1,17 @@
-class ModelComunicado:
-    pass
+from datetime import date
+
+from database import Base
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+
+class ModelComunicado(Base):
+    __tablename__ = "Comunicados"
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String(150), nullable=False)
+    conteudo = Column(Text, nullable=False)
+    destinatario_id = Column(Integer, ForeignKey("Usuarios.id"), nullable=False)
+    enviado_em = Column(Date, default=date.today, nullable=False)
+    ativo = Column(Boolean, default=True, nullable=False)
+    destinatario = relationship("ModelUsuario")
