1b24cfbba4ecbcb56551e3c36bbfcbf01263b25b
lucca3447 <lucca3447@hotmail.com>
2026-04-15 16:24:12 -0300

    feat: criado services

--- services/ServiceDisciplina.py
+++ services/ServiceDisciplina.py
@@ -1,2 +1,22 @@
+from sqlalchemy.orm import Session
+from repositories.RepositoryDisciplina import DisciplinaRepository
+
+
 class ServiceDisciplina:
-    pass
+    def __init__(self, db: Session):
+        self.repo = DisciplinaRepository(db)
+
+    def create(self, data):
+        return self.repo.create(data)
+
+    def get_by_id(self, obj_id: int):
+        return self.repo.get_byID(obj_id)
+
+    def get_all(self, skip: int = 0, limit: int = 100):
+        return self.repo.get_all(skip, limit)
+
+    def update(self, obj_id: int, data):
+        return self.repo.update(obj_id, data)
+
+    def delete(self, obj_id: int):
+        return self.repo.delete(obj_id)
