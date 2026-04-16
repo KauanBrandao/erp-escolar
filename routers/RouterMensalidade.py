be439138a28be22477eaeda1d98584971657a99d
Artur Campos Câmara <117042395+Artur0campos@users.noreply.github.com>
2026-04-16 11:16:40 -0300

    feat: criando Rotas de Mensalidade

---
+++ routers/RouterMensalidade.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from models.database import SessionLocal
+from schemas.SchemaMensalidade import (MensalidadeCreate, MensalidadeResponse,
+                                       MensalidadeUpdate)
+from services.ServiceMensalidade import ServiceMensalidade
+
+router = APIRouter(prefix="/mensalidades", tags=["Mensalidade"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/", response_model=MensalidadeResponse, status_code=201)
+def criar_aluno(dados: MensalidadeCreate, db: Session = Depends(get_db)):
+    return ServiceMensalidade(db).criar(dados)
+
+
+@router.get("/", response_model=list[MensalidadeResponse])
+def listar_alunos(db: Session = Depends(get_db)):
+    return ServiceMensalidade(db).listar()
+
+
+@router.get("/{mensalidade_id}", response_model=MensalidadeResponse)
+def buscar_aluno(mensalidade_id: int, db: Session = Depends(get_db)):
+    return ServiceMensalidade(db).buscar_por_id(mensalidade_id)
+
+
+@router.put("/{mensalidade_id}", response_model=MensalidadeResponse)
+def atualizar_aluno(mensalidade_id: int, dados: MensalidadeUpdate, db: Session = Depends(get_db)):
+    return ServiceMensalidade(db).atualizar(mensalidade_id, dados)
+
+
+@router.delete("/{mensalidade_id}")
+def deletar_aluno(mensalidade_id: int, db: Session = Depends(get_db)):
+    return ServiceMensalidade(db).deletar(mensalidade_id)
