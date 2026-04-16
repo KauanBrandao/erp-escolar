a61e7722600baadb5963efd4af6361d561518ab6
Artur Campos Câmara <117042395+Artur0campos@users.noreply.github.com>
2026-04-16 11:40:06 -0300

    feat: criando Rotas de Pagamentos

---
+++ routers/RouterPagamento.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from models.database import SessionLocal
+from schemas.SchemaPagamento import (PagamentoCreate, PagamentoResponse,
+                                     PagamentoUpdate)
+from services.ServicePagamento import ServicePagamento
+
+router = APIRouter(prefix="/pagamentos", tags=["Pagamentos"])
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/", response_model=PagamentoResponse, status_code=201)
+def criar_aluno(dados: PagamentoCreate, db: Session = Depends(get_db)):
+    return ServicePagamento(db).criar(dados)
+
+
+@router.get("/", response_model=list[PagamentoResponse])
+def listar_alunos(db: Session = Depends(get_db)):
+    return ServicePagamento(db).listar()
+
+
+@router.get("/{pagamento_id}", response_model=PagamentoResponse)
+def buscar_aluno(pagamento_id: int, db: Session = Depends(get_db)):
+    return ServicePagamento(db).buscar_por_id(pagamento_id)
+
+
+@router.put("/{pagamento_id}", response_model=PagamentoResponse)
+def atualizar_aluno(pagamento_id: int, dados: PagamentoUpdate, db: Session = Depends(get_db)):
+    return ServicePagamento(db).atualizar(pagamento_id, dados)
+
+
+@router.delete("/{pagamento_id}")
+def deletar_aluno(pagamento_id: int, db: Session = Depends(get_db)):
+    return ServicePagamento(db).deletar(pagamento_id)
